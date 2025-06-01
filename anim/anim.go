bd100a68ff646ca0eaaf5962d6847245d121ea6c
Votline <quogg2@gmail.com>
2025-06-01 22:40:14 +0300

    Changed jump animation

--- anim/anim.go
+++ anim/anim.go
@@ -1,14 +1,24 @@
 package anim
 
 import (
+	"math"
+
 	"github.com/go-gl/mathgl/mgl32"
 
 	"FirstHero/primShapes"
 )
 
+const (
+	AnimDeath = iota
+	AnimJump
+	AnimWalk
+	AnimIdle
+)
+
 type Animator struct {
 	animProgress float32
 	animSpeed float32
+
 }
 
 func NewAnimator() *Animator {
@@ -27,7 +37,7 @@ func (a *Animator) StartJump(limbs []*primShapes.Limb) {
 
 	for _, limb := range limbs {
 		if limb.Parent != nil && limb.Name != "Head" {
-			a.animateLeg(limb)
+			a.animateJump(limb)
 		}
 	}
 }
@@ -39,27 +49,51 @@ func (a *Animator) StopJump(limbs []*primShapes.Limb) {
 
 	for _, limb := range limbs {
 		if limb.Parent != nil && limb.Name != "Head" {
-			a.animateLeg(limb)
+			a.animateJump(limb)
 		} 
 	}
 }
 
-func (a *Animator) animateLeg(limb *primShapes.Limb) {
+func (a *Animator) animateJump(limb *primShapes.Limb) {
 	var yOffset float32
+	a.animSpeed = 0.008 
 	if a.animProgress <= 0.3 {
-		// Фаза отталкивания (0 → +0.1)
+		// Repulsion phase (0 -> +0.1)
 		yOffset = 0.01 * (a.animProgress / 0.3)
 	} else if a.animProgress <= 0.7 {
-		// Фаза полета (+0.1 → -0.05)
+		// Fly phase (+0.1 -> -0.05)
 		yOffset = 0.01 - 0.015*((a.animProgress-0.3)/0.4)
 	} else {
-		// Фаза приземления (-0.05 → -0.15)
+		// Landing phase (-0.05 -> -0.15)
 		yOffset = -0.005 - 0.01*((a.animProgress-0.7)/0.3)
 	}
 	for i := range limb.CurrentPos {
 		basePos := limb.Parent.CurrentPos[i].Add(limb.TargetPos[i])
 		limb.CurrentPos[i] = mgl32.Vec3{
 			basePos.X(), basePos.Y() + yOffset, basePos.Z(),
+		}
+	}
+
+	a.animSpeed = 0.03 
+
+	if limb.Name == "LeftHand" || limb.Name == "RightHand" {
+		angle := float32(0)
+		if a.animProgress > 0.2 {
+			targetAngle := float32(math.Pi/12)
+
+			if limb.Name == "LeftHand" {
+				angle = targetAngle * a.animProgress
+			} else {
+				angle = -targetAngle * a.animProgress
+			}
+		}
+		rotationCenter := limb.CurrentPos[0]
+		rotMat := mgl32.HomogRotate3DZ(angle)
+
+		for i := range limb.CurrentPos {
+			point := limb.CurrentPos[i].Sub(rotationCenter)
+			rotated := rotMat.Mul4x1(mgl32.Vec4{point.X(), point.Y(), point.Z(), 1})
+			limb.CurrentPos[i] = mgl32.Vec3{rotated.X(), rotated.Y(), rotated.Z()}.Add(rotationCenter)
 		}
 	}
 }
